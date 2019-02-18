45f2d6590c8c5833a4b90bfb913d7a9a7782247f
Withee Poositasai <witheep@gmail.com>
2019-02-18 21:34:13 +0700

    Add temp mechanic

--- main.js
+++ main.js
@@ -2,13 +2,37 @@ const fs = require('fs')
 const glob = require('glob')
 const unzipper = require('unzipper')
 const { unrar } = require('unrar-promise')
+const { ncp } = require('ncp')
 
-function extractRars(files, path, removeSource = false) {
+function getStudentDir(path) {
+  let paths = path.split('/')
+  return paths[0]
+}
+
+function getCurrentDir(path) {
+  let paths = path.split('/')
+  paths.splice(paths.length-1, 1)
+  return paths.join('/')
+}
+
+function copyAll(src, dst) {
+  return new Promise((resolve, reject) => {
+    ncp(src, dst, function (err) {
+      if (err) {
+        return console.error(err);
+        reject(err)
+      }
+      resolve()
+     })
+  })
+}
+
+function extractRars(files, removeSource = false) {
   let left = files.length
 
   return new Promise((resolve, reject) => {
     files.forEach(file => {
-      unrar(file, './output').then(() => {
+      unrar(file, getCurrentDir(file)).then(() => {
         left--
 
         if (removeSource) {
@@ -23,13 +47,13 @@ function extractRars(files, path, removeSource = false) {
   })
 }
 
-function extractZips(files, path, removeSource = false) {
+function extractZips(files, removeSource = false) {
   let left = files.length
 
   return new Promise((resolve, reject) => {
     files.forEach(file => {
       fs.createReadStream(file)
-        .pipe(unzipper.Extract({ path: path }))
+        .pipe(unzipper.Extract({ path: getCurrentDir(file) }))
         .on('close', () => {
           left--
 
@@ -45,31 +69,54 @@ function extractZips(files, path, removeSource = false) {
   })
 }
 
+function moveFiles(files, path) {
+  return new Promise((resolve, reject) => {
+    files.forEach(file => {
+      fpath = file.split('/')
+      fs.renameSync(file, path+'/'+fpath[fpath.length-1])
+    })
+
+    resolve()
+  })
+}
+
 async function main() {
-  if (process.argv.length <= 3) {
-    console.log("Usage: " + __filename + " input_path output_path");
+  if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " assesment_config_file");
     process.exit(-1);
   }
+
+  const assConfig = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
+
+  console.log('Creating temp...')
+
+  await copyAll(assConfig.inputPath, 'temp')
   
-  const inputPath = process.argv[2]
-  const outputPath = process.argv[3]
-  
-  let zipFiles = glob.sync(inputPath+'/**/*.zip')
-  let rarFiles = glob.sync(inputPath+'/**/*.rar')
-  let removeSource = false
+  let zipFiles = glob.sync('temp'+'/**/*.zip')
+  let rarFiles = glob.sync('temp'+'/**/*.rar')
+  let removeSource = true
+
+  // zipFiles.forEach(file => {
+  //   console.log(getCurrentDir(file))
+  // })
+
 
   while (zipFiles.length > 0) {
     console.log('Extracting '+zipFiles.length+' zip files + '+rarFiles.length+' rar files ...')
 
-    await extractZips(zipFiles, outputPath, removeSource)
-    await extractRars(rarFiles, outputPath, removeSource)
+    await extractZips(zipFiles, removeSource)
+    await extractRars(rarFiles, removeSource)
 
-    zipFiles = glob.sync(outputPath+'/**/*.zip')
-    rarFiles = glob.sync(outputPath+'/**/*.rar')
-    removeSource = true
+    zipFiles = glob.sync('temp'+'/**/*.zip')
+    rarFiles = glob.sync('temp'+'/**/*.rar')
   }
+
+  assConfig.tasks.forEach(task => {
+    let files = glob.sync('temp/**/*'+task+'*.*')
+    console.log(files)
+  })
   
-  // console.log(glob.sync(outputPath+'/**/*.c'))
+  
 }
 
 
